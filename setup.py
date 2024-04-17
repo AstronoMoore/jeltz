62d8395bbcc8c87a74911a79221c5423f198d5b8
Thomas Moore <80706958+AstronoMoore@users.noreply.github.com>
2024-04-17 15:00:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jeltz',
+    version='{{VERSION_PLACEHOLDER}}',
     version='0.1',
     packages=find_packages(),
     install_requires=[
