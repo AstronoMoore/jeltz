1458cfa8bdd9a35464c3f35279ba32a41666f926
Thomas Moore <80706958+AstronoMoore@users.noreply.github.com>
2024-04-17 16:38:39 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='jeltz',
     version='{{VERSION_PLACEHOLDER}}',
-    version='0.1',
     packages=find_packages(),
     install_requires=[
         'pandas',
